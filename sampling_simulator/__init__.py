0ae04ccd115c10ccf2be6663d0a67ca38593df4d
Wei-Tse Hsu <wehs7661@colorado.edu>
2023-09-04 18:26:05 -0600

    Modified sampling_simulator/__init__.py to fixed ImportError in CI

--- sampling_simulator/__init__.py
+++ sampling_simulator/__init__.py
@@ -1,12 +1,1 @@
-"""A python package for simulating the sampling behaviors of enhanced sampling simulations"""
-
-# Add imports here
-# from .sampling_simulator import *
-
-# Handle versioneer
-from ._version import get_versions  # noqa: ABS101
-
-versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
-del get_versions, versions
+from ._version import __version__
